e6764b739911d7741cd8386b57e5bfe5c097e862
Nimesh Maharjan <maharjannimesh11@gmail.com>
2024-01-31 11:37:52 +0545

    ci: change of directories

--- src/shared/lib/dictionary.ts
+++ src/shared/lib/dictionary.ts
@@ -2,10 +2,12 @@ import type { Locale } from '@/i18n.config';
 import 'server-only';
 
 const dictionaries = {
-  en: () => import('@/dictionaries/en.json').then((module) => module.default),
-  de: () => import('@/dictionaries/de.json').then((module) => module.default),
+  en: () =>
+    import('@/shared/dictionaries/en.json').then((module) => module.default),
+  de: () =>
+    import('@/shared/dictionaries/de.json').then((module) => module.default),
   ne: () =>
-    import('@/dictionaries/ne_NP.json').then((module) => module.default),
+    import('@/shared/dictionaries/ne_NP.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
